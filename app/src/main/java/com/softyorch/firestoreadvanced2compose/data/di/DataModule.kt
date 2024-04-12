9dce8bc262936ec2aa02831281bb03ea02a84d87
Jorge1 <agullojorge@gmail.com>
2024-04-12 21:18:26 +0200

    Created DI

---
+++ app/src/main/java/com/softyorch/firestoreadvanced2compose/data/di/DataModule.kt
@@ -0,0 +1,18 @@
+package com.softyorch.firestoreadvanced2compose.data.di
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Singleton
+    @Provides
+    fun providesFirestore() = Firebase.firestore
+}
