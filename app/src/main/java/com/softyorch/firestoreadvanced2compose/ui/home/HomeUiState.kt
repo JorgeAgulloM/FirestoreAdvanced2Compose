3e1c85851d64eb2b02acf1c6553701af53dc0bc5
Jorge1 <agullojorge@gmail.com>
2024-04-12 21:19:49 +0200

    Changed start value

--- app/src/main/java/com/softyorch/firestoreadvanced2compose/ui/home/HomeUiState.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced2compose/ui/home/HomeUiState.kt
@@ -3,7 +3,7 @@ package com.softyorch.firestoreadvanced2compose.ui.home
 import com.softyorch.firestoreadvanced2compose.domain.model.TransactionModel
 
 data class HomeUiState(
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val transactions: List<TransactionModel> = emptyList(),
     val totalAmount: String? = null,
 )
