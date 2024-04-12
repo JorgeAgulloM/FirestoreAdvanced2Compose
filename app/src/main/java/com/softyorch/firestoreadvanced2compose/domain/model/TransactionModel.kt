8f8be5d151d12152f2e26657550521fc127d1a9b
Jorge1 <agullojorge@gmail.com>
2024-04-12 21:19:30 +0200

    Changed types and added mapper

--- app/src/main/java/com/softyorch/firestoreadvanced2compose/domain/model/TransactionModel.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced2compose/domain/model/TransactionModel.kt
@@ -1,8 +1,12 @@
 package com.softyorch.firestoreadvanced2compose.domain.model
 
 data class TransactionModel(
-    val id: Int,
+    val id: String,
     val title: String,
-    val amount: String,
+    val amount: Double,
     val date: String,
-)
+) {
+    companion object {
+        fun TransactionModel.amountFormatted(): String = String.format("%.2f", this.amount)
+    }
+}
