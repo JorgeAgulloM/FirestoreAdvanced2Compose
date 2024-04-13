7ba52c576bde7c04995239e5c78282bba40c2903
Jorge1 <agullojorge@gmail.com>
2024-04-13 19:34:38 +0200

    Split show/dismiss dialog & implement newTransaction

--- app/src/main/java/com/softyorch/firestoreadvanced2compose/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced2compose/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.softyorch.firestoreadvanced2compose.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softyorch.firestoreadvanced2compose.data.network.DatabaseRepository
+import com.softyorch.firestoreadvanced2compose.domain.dto.TransactionDTO.Companion.prepareDTO
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -29,8 +30,21 @@ class HomeViewModel @Inject constructor(private val db: DatabaseRepository) : Vi
         }
     }
 
-    fun onAddNewTransaction(show: Boolean) {
-        _uiState.update { it.copy(showAddTransaction = show) }
+    fun onShowTransactionDialog() {
+        _uiState.update { it.copy(showAddTransaction = true) }
+    }
+
+    fun onDismissTransactionDialog() {
+        _uiState.update { it.copy(showAddTransaction = false) }
+    }
+
+    fun onAddNewTransaction(title: String, amount: String, date: Long?) {
+        prepareDTO(title, amount, date)?.let { dto ->
+            viewModelScope.launch {
+                db.addTransaction(dto)
+            }
+        }
+        onDismissTransactionDialog()
     }
 
 }
