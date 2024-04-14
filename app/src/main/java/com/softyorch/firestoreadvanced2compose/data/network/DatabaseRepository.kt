8bd0d87029b3c76e8471a2e9596938d400445de5
Jorge1 <agullojorge@gmail.com>
2024-04-14 10:17:43 +0200

    Implemented get remote config test

--- app/src/main/java/com/softyorch/firestoreadvanced2compose/data/network/DatabaseRepository.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced2compose/data/network/DatabaseRepository.kt
@@ -3,19 +3,28 @@ package com.softyorch.firestoreadvanced2compose.data.network
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.snapshots
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.softyorch.firestoreadvanced2compose.data.response.TransactionResponse
 import com.softyorch.firestoreadvanced2compose.data.response.TransactionResponse.Companion.toDomain
 import com.softyorch.firestoreadvanced2compose.domain.dto.TransactionDTO
 import com.softyorch.firestoreadvanced2compose.domain.model.TransactionModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class DatabaseRepository @Inject constructor(private val db: FirebaseFirestore) {
+class DatabaseRepository @Inject constructor(
+    private val db: FirebaseFirestore,
+    private val remoteConfig: FirebaseRemoteConfig
+) {
 
     companion object {
         const val USER_COLLECTION = "yorch"
         const val FILE_DATE = "date"
+
+        // Remote config params
+        const val TITLE_PARAM = "title"
+        const val FELIZ_PARAM = "feliz"
     }
 
     fun getTransactions(): Flow<List<TransactionModel>> {
@@ -44,6 +53,17 @@ class DatabaseRepository @Inject constructor(private val db: FirebaseFirestore) 
 
     fun deleteTransaction(id: String) {
         db.collection(USER_COLLECTION).document(id).delete()
+    }
+
+    suspend fun getAppInfo(): String {
+        // Solo para obtener valores urgentes, y nunca usar 0 seg.
+        remoteConfig.fetch(0)
+        remoteConfig.activate().await()
+        //
+
+        val title = remoteConfig.getString(TITLE_PARAM)
+        val feliz = remoteConfig.getBoolean(FELIZ_PARAM)
+        return "El título $title está feliz?: $feliz"
     }
 
 }
